ef606b53a0857d40f52148de98841fee520b2dc9
Roland <mehesroli@gmail.com>
2025-04-06 12:30:21 +0300

    adding some style , and some functionality

--- index.js
+++ index.js
@@ -1,99 +1,104 @@
-const startBtn = document.getElementById("startBtn");
-const prevStep = document.getElementById("prevStep");
-const nextStep = document.getElementById("nextStep");
-const playBtn = document.getElementById("playBtn");
-const container = document.getElementById("animation");
-
+const startBtn = document.getElementById('startBtn');
+const prevStep = document.getElementById('prevStep');
+const nextStep = document.getElementById('nextStep');
+const playBtn = document.getElementById('playBtn');
+const container = document.getElementById('animation');
 
 const animationSteps = [];
 let step = 0;
 let tick;
 
 const renderList = (list) => {
-    container.innerHTML = "";
-    list.forEach((no, index) => {
-        const box = document.createElement("div");
-        box.className = "list-element";
-        box.style.left = `${index * 30}px`
-        box.style.height = `${(no*2) + 20}px`
-        box.innerText = no;
-        container.appendChild(box);
-    })
-}
+  container.innerHTML = '';
+  list.forEach((no, index) => {
+    const box = document.createElement('div');
+    box.className = 'list-element';
+    box.style.left = `${index * 30}px`;
+    box.style.height = `${no * 2 + 20}px`;
+    box.style.bottom = `0px`;
+    box.innerText = no;
+    container.appendChild(box);
+  });
+};
 
-const generateList = (length=10, max=100, min=0) => {
-    const resultsArr = [];
+const generateList = (length = 10, max = 100, min = 0) => {
+  const resultsArr = [];
 
-    for (let i=0; i<length; i++) {
-        const newNumber = Math.floor(Math.random() * (max - min)) + min;
+  for (let i = 0; i < length; i++) {
+    const newNumber = Math.floor(Math.random() * (max - min)) + min;
 
-        if(resultsArr.includes(newNumber)){
-            length += 1;
-        }
-        else {
-            resultsArr.push(newNumber)
-        }
+    if (resultsArr.includes(newNumber)) {
+      length += 1;
+    } else {
+      resultsArr.push(newNumber);
     }
+  }
 
-    return resultsArr;
-}
+  return resultsArr;
+};
 
 const bubbleSort = (list) => {
-    const arr = [...list];
-    let swapped = true;
-    do {
-        swapped = false;
-        for(let i=0; i<arr.length; i++){
-            if(arr[i] > arr[i + 1]){
-                const temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-                swapped = true;
-                animationSteps.push([...arr]);
-            }
-        }
-    } while(swapped)
-    return arr;
-}
-
+  const arr = [...list];
+  let swapped = true;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        const temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+        animationSteps.push([...arr]);
+      }
+    }
+  } while (swapped);
+  return arr;
+};
 
 const main = () => {
-    const randomNumbers = generateList();
-    const maxValue = bubbleSort(randomNumbers)[randomNumbers.length-1];
-    container.style.height = `${(maxValue*2) + 20}px`
-    renderList(randomNumbers);
-}
+  const randomNumbers = generateList();
+  const maxValue = bubbleSort(randomNumbers)[randomNumbers.length - 1];
+  container.style.height = `${maxValue * 2 + 20}px`;
+  renderList(randomNumbers);
+};
 
 const animatePosition = (stepArray) => {
-    Array.from(container.children).forEach((child, index) => {
-        const value = Number(child.innerText);
-        const position = stepArray.indexOf(value);
-        child.style.left = `${position * 30}px`;
-    })
-}
+  Array.from(container.children).forEach((child, index) => {
+    const value = Number(child.innerText);
+    const position = stepArray.indexOf(value);
+    child.style.left = `${position * 30}px`;
+  });
+};
 
 const handleNextAnimStep = () => {
-    if(step > animationSteps.length - 1){
-        if(tick){
-            clearInterval(tick);
-            tick = null;
-        }
-        return;
+  if (step >= animationSteps.length) {
+    if (tick) {
+      clearInterval(tick);
+      tick = null;
     }
-    animatePosition(animationSteps[step++]);
-}
+    playBtn.disabled = false;
+    return;
+  }
+  animatePosition(animationSteps[step++]);
+};
 
 const handlePrevAnimStep = () => {
-    if(step === 0)return;
-    animatePosition(animationSteps[step--]);
-}
+  if (step === 0) return;
+  animatePosition(animationSteps[--step]);
+};
 
+startBtn.addEventListener('click', () => {
+  animationSteps.length = 0;
+  step = 0;
+  main();
+});
 
-startBtn.addEventListener("click", main);
-prevStep.addEventListener("click", handlePrevAnimStep);
-nextStep.addEventListener("click", handleNextAnimStep);
-playBtn.addEventListener("click", () => {
-    tick = setInterval(handleNextAnimStep, 1000);
-})
+prevStep.addEventListener('click', handlePrevAnimStep);
+nextStep.addEventListener('click', handleNextAnimStep);
 
-window.addEventListener("load", main);
+playBtn.addEventListener('click', () => {
+  playBtn.disabled = true;
+  tick = setInterval(handleNextAnimStep, 1000);
+});
+
+window.addEventListener('load', main);
